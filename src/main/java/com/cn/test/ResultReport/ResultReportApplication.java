2a30c2889b911ffd2454070345fcfcfd2bd06fdf
Cirbles <liguangchun1990@foxmail.com>
2020-01-09 09:44:57 +0800

    添加一个新建立的空项目

---
+++ src/main/java/com/cn/test/ResultReport/ResultReportApplication.java
@@ -0,0 +1,13 @@
+package com.cn.test.ResultReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cn/test/ResultReport/ResultReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.test.ResultReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
